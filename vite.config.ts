be874513c22f0a410bbf9796cf011db84ecc10ff
toppo09 <95916639+toppo09@users.noreply.github.com>
2026-06-09 01:54:19 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MEDISIS/', // ★リポジトリ名をスラッシュで挟んで指定
+  base: '/MEDISIS/', 
 })
